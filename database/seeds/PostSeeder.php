c9e1c734eaa2682439dfe4d91f1ce849fbe5aaf5
AlbertoBiga <alberto.biga10@gmail.com>
2022-06-07 16:44:47 +0200

    Definito seeder e avviato db:seed

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Models\Post;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class PostSeeder extends Seeder
 {
@@ -11,8 +12,15 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ( $i=0; $i < 10; $i++ ){
+            $post = new Post();
+            $post->title = $faker->text();
+            $post->content = $faker->paragraph(2);
+            $post->image = $faker->imageUrl(250, 250);
+            $post->slug = Str::slug($post->title, '-');
+            $post->save();
+        }
     }
 }
